18261a3ba63b2a870f1a9a437c9205d8fa89b43a
Antonio Marques <antonio@quimbik.com>
2024-07-04 20:45:19 -0300

    added tests for postService

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -2,85 +2,62 @@ import { API_URL } from "../constants";
 
 
 async function createPost(postData) {
-    try {
-        const response = await fetch(API_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(postData),
-        });
-        if (response.ok) {
-            return await response.json();
-        } else {
-            throw response;
-        }
-    } catch (e) {
-        console.log("An error occurred: ", e);
-  }
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+    });
+    if (response.ok) {
+        return await response.json();
+    } else {
+        throw new Error(response.statusText)
+    }
 }
 
 async function fetchAllPosts() {
-    try {
-        const response = await fetch(API_URL);
-        if (response.ok) {
-            return await response.json();
-        } else {
-            throw response;
-        }
-    } catch (e) {
-        console.log("An error occurred: ", e);
+    const response = await fetch(API_URL);
+    if (response.ok) {
+        return await response.json();
+    } else {
+        throw new Error(response.statusText)
     }
 }
 
 async function fetchPost(id) {
-    try {
-        const response = await fetch(`${API_URL}/${id}`);
-        if (response.ok) {
-            return await response.json();
-        } else {
-            throw response;
-        }
-    } catch (e) {
-        console.log("An error occurred: ", e);
-    }
+    const response = await fetch(`${API_URL}/${id}`);
+    if (response.ok) {
+        return await response.json();
+    } else {
+        throw new Error(response.statusText)
+    }    
 }
 
 async function deletePost(id) {
-    try {
-        const response = await fetch(`${API_URL}/${id}`, {
-            method: "DELETE",
-        });
-        if (!response.ok) {
-            throw new Error(response.statusText)
-        }
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+    });
 
-        if (response.status === 204) {
-            return null;
-        }
-
-        return await response.json();
-    } catch (e) {
-        console.log("An error occurred: ", e);
+    if (response.status === 204) {
+        return null;
     }
+
+    throw new Error(response.statusText)
 }
 
 async function updatePost(id, postData) {
-    try {
-        const response = await fetch(`${API_URL}/${id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(postData),
-        });
-        if (response.ok) {
-            return await response.json();
-        } else {
-            throw response;
-        }
-    } catch (e) {
-        console.log("An error occurred: ", e);
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+    });
+    if (response.ok) {
+        return await response.json();
+    } else {
+        throw new Error(response.statusText)
     }
 }
 export { createPost, deletePost, fetchAllPosts, fetchPost, updatePost };
