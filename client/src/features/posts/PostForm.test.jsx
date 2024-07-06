ec8da39e2d0c833905c5f931bb4adcf23034fa99
Antonio Marques <antonio@quimbik.com>
2024-07-05 21:06:24 -0300

    created test for PostForm

---
+++ client/src/features/posts/PostForm.test.jsx
@@ -0,0 +1,74 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import PostForm from './PostForm';
+import { act } from 'react';
+
+describe('PostForm', () => {
+    // post is null by default, for the instance when a new post is being created
+    it('renders default inputs when no post prop is passed', () => {
+        const mockSubmit = jest.fn();
+        const buttonText = "Submit";
+        const { getByLabelText } = render(<PostForm headerText="New Post" onSubmit={mockSubmit} buttonText={buttonText}/>);
+        expect(getByLabelText('Title:')).toBeInTheDocument();
+        expect(getByLabelText('Body:')).toBeInTheDocument();
+
+    });
+
+    it('renders passed in post data', () => {
+        const mockPost = {
+            title: "Test Post",
+            body: "This is a test post"
+        }
+
+        const mockSubmit = jest.fn();
+        const buttonText = "Submit";
+        const { getByLabelText } = render(<PostForm post={mockPost} headerText="Edit Post" onSubmit={mockSubmit} buttonText={buttonText}/>);
+
+        expect(getByLabelText('Title:')).toBeInTheDocument();
+        expect(getByLabelText('Body:')).toBeInTheDocument();
+        expect(getByLabelText('Title:')).toHaveValue(mockPost.title);
+        expect(getByLabelText('Body:')).toHaveValue(mockPost.body);
+        
+    });
+
+    it('updates the input value on change', () => {
+        const mockPost = {
+            title: "Test Post",
+            body: "This is a test post"
+        }
+
+        const mockSubmit = jest.fn();
+        const buttonText = "Submit";
+        const { getByLabelText } = render(<PostForm post={mockPost} headerText="Edit Post" onSubmit={mockSubmit} buttonText={buttonText}/>);
+
+        fireEvent.change(getByLabelText('Title:'), { target: { value: "New Title" } });
+        fireEvent.change(getByLabelText('Body:'), { target: { value: "New Body" } });
+
+        expect(getByLabelText('Title:')).toHaveValue("New Title");
+        expect(getByLabelText('Body:')).toHaveValue("New Body");
+    })
+
+    it('calls onSubmit with the form data when submitted', async () => { 
+        const mockSubmit = jest.fn();
+        const buttonText = "Submit";
+        const headerText = "New Post";
+        const { getByLabelText, getByRole } = render(
+        <PostForm headerText={headerText} onSubmit={mockSubmit} buttonText={buttonText}/>);
+
+        const titleInput = getByLabelText('Title:');
+        const bodyInput = getByLabelText('Body:');
+        const newTitle = "New Title";
+        const newBody = "New Body";
+
+        fireEvent.change(titleInput, { target: { value: newTitle } });
+        fireEvent.change(bodyInput, { target: { value: newBody } });
+
+        await act(async () => {
+            fireEvent.click(getByRole('button', { name: buttonText}));
+        });
+
+        expect(mockSubmit).toHaveBeenCalledTimes(1);
+        expect(mockSubmit).toHaveBeenCalledWith({ title: newTitle, body: newBody });
+
+    })
+
+});
